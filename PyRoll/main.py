50717ab1e626246d79315f15f56d1b148567aecd
Esaa Yamini <63874540+eyamini@users.noreply.github.com>
2020-05-27 14:13:15 -0500

    updated homework

---
+++ PyRoll/main.py
@@ -0,0 +1,43 @@
+import os
+import csv
+
+csvpath = 'election_data.csv';
+
+candidate_names = []
+candidate = []
+candidate_votes = []
+candidate_percent = []
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')   
+    header = next(csvreader)
+    #print(header)
+
+    total_votes = 0
+    candidate_total = 0
+
+    for row in csvreader:
+        total_votes += 1
+        
+        candidate_names.append(row[2])
+
+    for name in set(candidate_names):    
+        candidate.append(name)
+        candidate_total = candidate_names.count(name)
+        candidate_votes.append(candidate_total)
+        percent = (candidate_total/total_votes) * 100
+        candidate_percent.append(percent)
+
+    winner_count = max(candidate_votes)
+    winner_name = candidate[candidate_votes.index(winner_count)]
+
+
+    print("Election Results")
+    print("------------------------")
+    print("Total Votes: " + str(total_votes))
+    print("------------------------")
+    for row in range(len(candidate)):
+        print(candidate[row] + ":  " + str(candidate_percent) + "%" + "(" + str(candidate_votes[row]) + ")")
+    print("------------------------")   
+    print("Winner: " + str(winner_name))
+    print("------------------------")        
